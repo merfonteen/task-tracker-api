7605cbbf9144977ccb2849b8568da29127d432aa
Maxim Shelofast <maximshelofast@gmail.com>
2025-03-11 17:59:01 +0200

    fixed docker-compose.yml

--- src/main/java/by/sirius/task/tracker/Application.java
+++ src/main/java/by/sirius/task/tracker/Application.java
@@ -1,5 +1,6 @@
 package by.sirius.task.tracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +9,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @SpringBootApplication
 public class Application {
+
+    static {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
